0f7534d1b11fce864f64fd7776944c5a5d7952e3
ayoraheem <63825523+ayoraheem@users.noreply.github.com>
2020-05-01 21:48:44 -0500

    Add files via upload

---
+++ projec2.py
@@ -0,0 +1,20 @@
+# my stuff
+staff = [ "orange" , "apple", "1", "100"]
+print(staff)
+print(staff[-1])
+print(f" my best friut is {staff[0]}")
+# my friends list
+friends = [ "wale", "owolabi", "paul"]
+print(friends[0])
+print(f" these two are my best {friends[0]} {friends[1]}")
+friends[0]= "james"
+print(friends)
+# sort method
+cars = ["toyota", "bmw" , "benz"]
+cars .sort()
+print(cars)
+print(len(cars))
+letter = ["ABCDEFGH"]
+print(len(letter))
+for car in cars:
+    print(car)
